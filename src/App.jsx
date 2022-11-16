04e1454cd73ca1d7f964521164b711072be35b9c
jhansi <garipallyjhansi@gmail.com>
2022-11-16 13:53:41 +0200

    added cal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Nav } from "./Components/Nav.jsx";
+import { Cal } from "./Components/cal.jsx";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Cal />
     </div>
   );
 }
