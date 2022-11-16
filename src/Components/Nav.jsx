3d139b7293e6cdfe852d62f1715fad7e943deba3
Humera <humerabanoo987@gmail.com>
2022-11-16 12:33:24 +0200

    added nav

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,16 @@
+import "./Nav.css";
+
+export function Nav() {
+  return (
+    <div className="Nav">
+      <a href="#">Log In</a>
+      <a id="now" href="#">
+        Get Started
+      </a>
+      <a href="#">Resources</a>
+      <a href="#">About</a>
+      <a href="#">Marketplace</a>
+      <a href="#">Calculate</a>
+    </div>
+  );
+}
