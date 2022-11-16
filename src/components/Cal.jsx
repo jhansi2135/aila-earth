65fbff7af7d834f0a9674bdd5253da53cd430e14
jhansi <garipallyjhansi@gmail.com>
2022-11-16 13:53:57 +0200

    added cal

---
+++ src/components/Cal.jsx
@@ -0,0 +1,49 @@
+import "./Cal.css";
+import Learn from "./Learn.png";
+import Attract from "./Attract.png";
+import Act from "./Act.png";
+
+export function Cal() {
+  return (
+    <div className="cal">
+      <p id="num"> 186000 t. </p>
+      <p id="num">co2e reduced by Aila </p>
+
+      <p id="num1">How does Aila platform work</p>
+      <div className="icon">
+        <div className="icon1">
+          <img src={Learn} type=".png" alt="icon" />
+          <br /> <br />
+          <a href="#">Learn</a>
+          <p>
+            With automated emission accounting protocol. Results reveal the
+            climate health index of the company.
+          </p>
+        </div>
+
+        <div className="icon1">
+          <a href="#Market">
+            <img src={Act} type=".png" alt="icon" />
+            <br /> <br />
+            Act
+          </a>
+          <p>
+            By providing reports and omni-channel shareable content to
+            communicate the actions taken. Aila also tracks the SME's climate
+            impact overtime.
+          </p>
+        </div>
+
+        <div className="icon1">
+          <img src={Attract} type=".png" alt="icon" />
+          <br /> <br />
+          <a href="#">Attract</a>
+          <p>
+            Through a marketplace that offers a fast and efficient way to shrink
+            carbon footprint by switching to green goods and services.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
