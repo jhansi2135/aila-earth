1939d7c04e7bc0484fe67df6963f5b47a98c9466
Humera <humerabanoo987@gmail.com>
2022-11-16 13:06:28 +0200

    added Image Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Nav } from "./Components/Nav.jsx";
+import { Image } from "./Components/Image.jsx";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Image />
     </div>
   );
 }
---
+++ src/Components/Image.jsx
@@ -0,0 +1,26 @@
+import "./Image.css";
+
+export function Image() {
+  return (
+    <div className="Image">
+      <h1>
+        Small Effort,
+        <br />
+        Big Impact
+      </h1>
+      <p>
+        <b>
+          Welcome to Aila, the first centralised platform <br />
+          helping SMEs to take direct climate actions
+          <br />
+          <br />
+          Start your green transition now, visit our{" "}
+          <a id="now" href="#">
+            marketplace
+          </a>
+          .
+        </b>
+      </p>
+    </div>
+  );
+}
