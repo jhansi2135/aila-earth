5da22d74d89671519b5114b5f7feafdd648ce4f5
Humera <humerabanoo987@gmail.com>
2022-11-18 20:39:34 +0200

    modified app.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/marketplace/i);
+  const linkElement = screen.getByText(/Small Effort,/i);
   expect(linkElement).toBeInTheDocument();
 });
