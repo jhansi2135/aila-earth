359819bef111d7284b173b878051f66cfab4b479
Humera <humerabanoo987@gmail.com>
2022-11-23 15:14:17 +0200

    added fetch.js

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,17 +1,19 @@
-const ENDPOINT_URL = "https://major-jennet-22.hasura.app/v1/graphql";
-const COMMENTS_QUERY = `
+const ENDPOINT_URL = "https://model-gnu-40.hasura.app/v1/graphql";
+const PARTNER_QUERY = `
     query {
-        comment {
-            title
-            content
+        Partner_Details {
+           id
+           partner_name
+           partner_companyName
+           partner_email 
         }
     }
 `;
-export const fetchComments = async () => {
+export const fetchPartners = async () => {
   const response = await fetch(ENDPOINT_URL, {
     method: "POST",
     body: JSON.stringify({
-      query: COMMENTS_QUERY,
+      query: PARTNER_QUERY,
     }),
   });
   return await response.json();
