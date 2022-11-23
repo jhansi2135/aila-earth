60708009eafd25d7ec018e462645596c679db1a8
Humera <humerabanoo987@gmail.com>
2022-11-23 12:56:58 +0200

    added LogIn using Google

--- src/Components/Google.jsx
+++ src/Components/Google.jsx
@@ -1,9 +1,20 @@
 import "./Google.css";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
 export function Google() {
   return (
     <div className="Google">
-      <button>SignIn using Google</button>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse.credential);
+          var decoded = jwt_decode(credentialResponse.credential);
+          console.log(decoded);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="672246395799-61t60k6v9rlpc28jj7kfr1dd2kp90po4.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
