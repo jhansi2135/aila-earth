928add5442d21bb9ed5018e1d2a904305c99c210
jhansi <garipallyjhansi@gmail.com>
2022-11-28 11:25:40 +0200

    c

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -5,14 +5,20 @@ import { Link } from "react-router-dom";
 export function LogIn() {
   return (
     <div className="LogIn">
+      <h1>Log in</h1>
+      <p>
+        Already a user? Log in to complete transactions and start making an
+        impact.
+      </p>
+
       <div id="one">
-        <h1>User LogIn</h1>
         <input
           type="text"
           name="email"
           placeholder="Enter your Email"
           required
         />
+        <br />
         <br />
         <input
           type="text"
@@ -21,28 +27,37 @@ export function LogIn() {
           required
         />
         <br />
+        <br />
         <Link to="">
           <input type="button" value="LogIn" />
         </Link>
-        <p id="simp">or</p>
-        <Google />
-        <Link to="/Register_User">
-          <p>Create Account (if you aren't registered)</p>
-        </Link>
+        <br />
+        <a href="#">Forgot your password?</a>
       </div>
       <div id="two">
-        <h1>Partner LogIn</h1>
-        <input type="text" name="email" placeholder="Enter your Email" />
-        <br />
-        <input type="text" name="password" placeholder="Enter your Password" />
-        <br />
-        <Link to="">
-          <input type="button" value="LogIn" />
-        </Link>
-        <p id="simp">or</p>
         <Google />
+      </div>
+      <div id="three">
+        <p>
+          By logging in you agree to our<a href="#">Terms of Use</a> and
+          acknowledge our<a href="#"> Privacy Policy</a>. You also acknowledge
+          that Aila uses cookies to give the best user experience.
+        </p>
+
+        <p>
+          This site is protected by reCAPTCHA Enterprise and Google
+          <a href="#"> Privacy Policy</a> and <a href="#">Terms of Use </a>
+          apply.
+        </p>
+        <p>
+          <b>
+            New to our platform? You can register in few easy steps and after
+            completing any transactions also utilise our reporting and
+            communication tools.
+          </b>
+        </p>
         <Link to="/Register_Partner">
-          <p>Create Account (if you aren't registered)</p>
+          <input type="button" value="REGISTER" />
         </Link>
       </div>
     </div>
