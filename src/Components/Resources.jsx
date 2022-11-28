2ce95b5fdcd63f60e6c18f21c0cf2557496f67b9
Humera <humerabanoo987@gmail.com>
2022-11-28 20:25:24 +0200

    add Resources

--- src/Components/Resources.jsx
+++ src/Components/Resources.jsx
@@ -1,10 +1,12 @@
+import "./Resources.css";
+
 export function Resources() {
   return (
     <div className="Resources">
       <p>Something Awesome Is In The Works</p>
       <p>We will be up and running soon with the new and improved website.</p>
       <p>We love feedback!</p>
-      <button>CONTACT US</button>
+      <button id="btn-r">CONTACT US</button>
     </div>
   );
 }
