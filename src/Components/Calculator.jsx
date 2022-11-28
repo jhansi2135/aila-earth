9be69f70a079627a6af5cadde99259cc5d83ae4f
Humera <humerabanoo987@gmail.com>
2022-11-28 20:22:02 +0200

    added Calculator

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,10 +1,12 @@
+import "./Calculator.css";
+
 export function Calculator() {
   return (
     <div className="Calculator">
       <p>Something Awesome Is In The Works</p>
       <p>We will be up and running soon with the new and improved website.</p>
       <p>We love feedback!</p>
-      <button>CONTACT US</button>
+      <button id="btn-c">CONTACT US</button>
     </div>
   );
 }
