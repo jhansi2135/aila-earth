2f3f6a7375be290fbcef195044dabc32de62d093
Humera <humerabanoo987@gmail.com>
2022-12-05 14:21:11 +0200

    small correction

--- src/Components/ReadMoreAndAct.jsx
+++ src/Components/ReadMoreAndAct.jsx
@@ -44,102 +44,85 @@ export function ReadMoreAndAct(props, { userInfo, setUserInfo }) {
       );
     });
 
-  const handleUserInfoClick = () => {
-    setUserInfo.email_verified(false);
-  };
-
   return (
-    <>
-      <div className="user">
-        {userInfo && (
-          <div className="dropdown">
-            <button className="dropbtn">{userInfo.picture}</button>
-            <div className="dropdown-content">
-              <Link to="/order-summary">{userInfo.name}</Link>
-              <button onClick={handleUserInfoClick}>Log out</button>
-            </div>
-          </div>
-        )}
-      </div>
-      <div key={detail.id} className="ReadMoreAndAct">
-        <img className="img" src={detail.partner_companylogo} alt={props.alt} />
-        <div className="yks">
-          <div>
-            <h2 id="notfetch">{detail.partner_companyName}</h2>
-            <p>{detail.partner_tell_us}</p>
-          </div>
-          <div>
-            <h3>Product Category</h3>
-            <p id="fetch">{detail.partner_business_area}</p>
-          </div>
-          <div>
-            <h3>Product description</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sint
-              nobis ex ratione in soluta pariatur nisi iste dolore ullam quam
-              autem animi perspiciatis, doloremque, mollitia voluptatum
-              consequuntur esse molestias.
-            </p>
-          </div>
-          <div>
-            <h3>Carbon emission details</h3>
-          </div>
-          <div>
-            <h3>Pricing</h3>
-          </div>
-          <div>
-            <h3>Contract term</h3>
-          </div>
-          <div>
-            <h3>Standards for quality</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
-              reprehenderit rem nemo molestias ipsam quia illum aspernatur
-              debitis temporibus saepe velit iste deleniti ullam porro
-              blanditiis quod provident, eaque accusantium.
-            </p>
-          </div>
-          <div>
-            <button onClick={handleSubmit}>
-              Click to purchase and reduce your carbon footprint
-            </button>
-          </div>
-          <div>
-            <h3>Browse other products from {detail.partner_companyName}</h3>
-          </div>
-          <div>
-            <h3>
-              Browse products from same category: {detail.partner_business_area}
-            </h3>
-            <div className="cards">
-              {details &&
-                fetchList.map(
-                  (detail) =>
-                    detail.partner_confirmation && (
-                      <div key={detail.id} className="card">
-                        <img src={detail.partner_companylogo} alt={props.alt} />
-                        <div className="p">
-                          <p className="p1">{detail.partner_companyName}</p>
-                          <p className="p2">{detail.partner_business_area}</p>
-                          <p className="p3">{detail.partner_tell_us}</p>
-                          <Link
-                            onClick={() => window.location.reload(false)}
-                            to="/ReadMoreAndAct"
-                            state={{ detail: detail }}
-                          >
-                            Read more & Act
-                          </Link>
-                        </div>
+    <div key={detail.id} className="ReadMoreAndAct">
+      <img className="img" src={detail.partner_companylogo} alt={props.alt} />
+      <div className="yks">
+        <div>
+          <h2 id="notfetch">{detail.partner_companyName}</h2>
+          <p>{detail.partner_tell_us}</p>
+        </div>
+        <div>
+          <h3>Product Category</h3>
+          <p id="fetch">{detail.partner_business_area}</p>
+        </div>
+        <div>
+          <h3>Product description</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sint
+            nobis ex ratione in soluta pariatur nisi iste dolore ullam quam
+            autem animi perspiciatis, doloremque, mollitia voluptatum
+            consequuntur esse molestias.
+          </p>
+        </div>
+        <div>
+          <h3>Carbon emission details</h3>
+        </div>
+        <div>
+          <h3>Pricing</h3>
+        </div>
+        <div>
+          <h3>Contract term</h3>
+        </div>
+        <div>
+          <h3>Standards for quality</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
+            reprehenderit rem nemo molestias ipsam quia illum aspernatur debitis
+            temporibus saepe velit iste deleniti ullam porro blanditiis quod
+            provident, eaque accusantium.
+          </p>
+        </div>
+        <div>
+          <button onClick={handleSubmit}>
+            Click to purchase and reduce your carbon footprint
+          </button>
+        </div>
+        <div>
+          <h3>Browse other products from {detail.partner_companyName}</h3>
+        </div>
+        <div>
+          <h3>
+            Browse products from same category: {detail.partner_business_area}
+          </h3>
+          <div className="cards">
+            {details &&
+              fetchList.map(
+                (detail) =>
+                  detail.partner_confirmation && (
+                    <div key={detail.id} className="card">
+                      <img src={detail.partner_companylogo} alt={props.alt} />
+                      <div className="p">
+                        <p className="p1">{detail.partner_companyName}</p>
+                        <p className="p2">{detail.partner_business_area}</p>
+                        <p className="p3">{detail.partner_tell_us}</p>
+                        <Link
+                          onClick={() => window.location.reload(false)}
+                          to="/ReadMoreAndAct"
+                          state={{ detail: detail }}
+                        >
+                          Read more & Act
+                        </Link>
                       </div>
-                    )
-                )}
-            </div>
+                    </div>
+                  )
+              )}
           </div>
         </div>
-        <Link to="/">
-          <img id="cross" src={Cross} alt="cross" />
-        </Link>
       </div>
-    </>
+      <Link to="/">
+        <img id="cross" src={Cross} alt="cross" />
+      </Link>
+    </div>
   );
 }
